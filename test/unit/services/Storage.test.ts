5949437276ae60d24967b7a7dd061d1b562a0aef
Dimitar Andreev <D.Andreev90@gmail.com>
2019-11-10 17:21:33 +0200

    Add DI (#20)

--- test/unit/services/Storage.test.ts
+++ test/unit/services/Storage.test.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import path from 'path';
+import path, {sep} from 'path';
 import Storage from '../../../src/services/Storage';
 import IStorage from '../../../src/services/interfaces/IStorage';
 
@@ -186,12 +186,12 @@ describe('Storage', () => {
         beforeEach(() => {
             mainPath = './path/to/app/';
             paths = [
-                'src/App',
+                `src${sep}App`,
                 'index',
-                'src/reducers/rootReducer',
-                'src/actions/simpleAction',
-                'src/reducers/simpleReducer',
-                'src/components/MyComponent'
+                `src${sep}reducers${sep}rootReducer`,
+                `src${sep}actions${sep}simpleAction`,
+                `src${sep}reducers${sep}simpleReducer`,
+                `src${sep}components${sep}MyComponent`
             ];
             // @ts-ignore
             path.join = require.requireActual('path').join;
@@ -216,9 +216,9 @@ describe('Storage', () => {
                 const cb = () => {
                     const expectedPaths: string[] = [
                         'src',
-                        'src/reducers',
-                        'src/actions',
-                        'src/components'
+                        `src${sep}reducers`,
+                        `src${sep}actions`,
+                        `src${sep}components`
                     ];
                     expectedPaths.forEach((currentPath: string, i: number) => {
                         expect(fs.existsSync).toHaveBeenCalledTimes(expectedPaths.length);
