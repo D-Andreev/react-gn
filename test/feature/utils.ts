5905dff5e993c1c2d0133e7febe0b03d94106fe4
Dimitar Andreev <dandreev@endava.com>
2019-11-27 10:57:12 +0200

    Do magic

--- test/feature/utils.ts
+++ test/feature/utils.ts
@@ -1,17 +1,20 @@
 import {execSync} from 'child_process';
 
-export function setupGit() {
+export function setupEnv() {
     const email = 'd.andreev90@gmail.com';
     const user = 'D-Andreev';
     execSync(`git config --global user.email "${email}"`);
     execSync(`git config --global user.name "${user}"`);
+    execSync('sudo chown -R $USER ~/.npm');
+    execSync('sudo chown -R $USER /usr/lib/node_modules');
+    execSync('sudo chown -R $USER /usr/local/lib/node_modules');
 }
 
 export function buildPackage() {
     if (process.env.TEST_ENV === 'CI') {
-        setupGit();
-        execSync('sudo npm link');
+        setupEnv();
         execSync('sudo npm install -g create-react-app');
+        execSync('sudo npm link');
     }
     execSync('yarn build');
 }
