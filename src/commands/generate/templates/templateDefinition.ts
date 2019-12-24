945b3cb7e9644a2aeab08c82ccf47458d59f3688
Dimitar Andreev <d.andreev90@gmail.com>
2019-12-25 00:07:35 +0200

    WIP

--- src/commands/generate/templates/templateDefinition.ts
+++ src/commands/generate/templates/templateDefinition.ts
@@ -5,20 +5,21 @@ const main: any = [
     {path: `${COMPONENT_NAME_PLACEHOLDER}.styles.ejs`, extension: 'css'},
     {path: `${COMPONENT_NAME_PLACEHOLDER}.test.ejs`, extension: 'js'}
 ];
+const styledComponents: any = [
+    {path: `Styled${COMPONENT_NAME_PLACEHOLDER}.ejs`, extension: 'js'}
+];
 
 const templateDefinition: any = {
     js: {
         component: {
             main,
             [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: [
-                {path: 'StyledButton.ejs', extension: 'js'}
+                {path: 'Styled{Component}.ejs', extension: 'js'}
             ]
         },
         container: {
             main,
-            [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: [
-                {path: 'StyledButton.ejs', extension: 'js'}
-            ],
+            [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: styledComponents,
             [TEMPLATE_OPTIONS.WITH_REDUX]: [
                 {path: './actions/simpleActions.ejs', extension: 'js'},
                 {path: './reducers/simpleReducer.ejs', extension: 'js'}
@@ -28,15 +29,11 @@ const templateDefinition: any = {
     ts: {
         component: {
             main,
-            [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: [
-                {path: 'StyledButton.ejs', extension: 'js'}
-            ]
+            [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: styledComponents,
         },
         container: {
             main,
-            [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: [
-                {path: 'StyledButton.ejs', extension: 'js'}
-            ],
+            [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: styledComponents,
             [TEMPLATE_OPTIONS.WITH_REDUX]: [
                 {path: './actions/simpleActions.ejs', extension: 'js'},
                 {path: './reducers/simpleReducer.ejs', extension: 'js'}
