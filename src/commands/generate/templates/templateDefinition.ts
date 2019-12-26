6c953554ae235c6833290c63700e2d4e16027720
Dimitar Andreev <d.andreev90@gmail.com>
2019-12-26 16:06:11 +0200

    Rename templates

--- src/commands/generate/templates/templateDefinition.ts
+++ src/commands/generate/templates/templateDefinition.ts
@@ -1,20 +1,20 @@
 import {COMPONENT_NAME_PLACEHOLDER, TEMPLATE_OPTIONS} from '../../../constants';
 
 const main: any = [
-    {path: `${COMPONENT_NAME_PLACEHOLDER}.ejs`, extension: 'jsx'},
-    {path: `${COMPONENT_NAME_PLACEHOLDER}.styles.ejs`, extension: 'css'},
-    {path: `${COMPONENT_NAME_PLACEHOLDER}.test.ejs`, extension: 'js'}
+    {path: `${COMPONENT_NAME_PLACEHOLDER}.ejs`},
+    {path: `${COMPONENT_NAME_PLACEHOLDER}.styles.ejs`},
+    {path: `${COMPONENT_NAME_PLACEHOLDER}.test.ejs`}
 ];
 const mainTs: any = [
-    {path: `${COMPONENT_NAME_PLACEHOLDER}.ejs`, extension: 'tsx'},
-    {path: `${COMPONENT_NAME_PLACEHOLDER}.styles.ejs`, extension: 'css'},
-    {path: `${COMPONENT_NAME_PLACEHOLDER}.test.ejs`, extension: 'ts'}
+    {path: `${COMPONENT_NAME_PLACEHOLDER}.ejs`},
+    {path: `${COMPONENT_NAME_PLACEHOLDER}.styles.ejs`},
+    {path: `${COMPONENT_NAME_PLACEHOLDER}.test.ejs`}
 ];
 const styledComponents: any = [
-    {path: `Styled${COMPONENT_NAME_PLACEHOLDER}.ejs`, extension: 'js'}
+    {path: `Styled${COMPONENT_NAME_PLACEHOLDER}.ejs`}
 ];
 const styledComponentsTs: any = [
-    {path: `Styled${COMPONENT_NAME_PLACEHOLDER}.ejs`, extension: 'ts'}
+    {path: `Styled${COMPONENT_NAME_PLACEHOLDER}.ejs`}
 ];
 const templateDefinition: any = {
     js: {
@@ -26,8 +26,8 @@ const templateDefinition: any = {
             main,
             [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: styledComponents,
             [TEMPLATE_OPTIONS.WITH_REDUX]: [
-                {path: './actions/actions.ejs', extension: 'js'},
-                {path: './reducers/reducer.ejs', extension: 'js'}
+                {path: './actions/actions.ejs'},
+                {path: './reducers/reducer.ejs'}
             ],
         }
     },
@@ -40,8 +40,8 @@ const templateDefinition: any = {
             main: mainTs,
             [TEMPLATE_OPTIONS.WITH_STYLED_COMPONENTS]: styledComponentsTs,
             [TEMPLATE_OPTIONS.WITH_REDUX]: [
-                {path: './actions/actions.ejs', extension: 'ts'},
-                {path: './reducers/reducer.ejs', extension: 'ts'}
+                {path: './actions/actions.ejs'},
+                {path: './reducers/reducer.ejs'}
             ],
         }
     }
